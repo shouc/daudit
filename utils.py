d4caf94307f7390d6407c8b2dc82a72aba5e3867
shouc <shou@ucsb.edu>
2020-06-08 07:32:18 -0700

    add whereis utils

--- utils.py
+++ utils.py
@@ -1,17 +1,39 @@
 import re
 import ipaddress
+import platform
+import subprocess
+import logs
 
 password_regex = re.compile('^.*(?=.{6,16})(?=.*\d)(?=.*[A-Z])(?=.*[a-z]{2,})(?=.*[!@#$%^&*?\(\)]).*$')
 
 
-def check_pwd(pwd):
+def check_pwd(pwd: str):
     if password_regex.match(pwd):
         return 1
     return 0
 
 
-def is_ip_internal(ip):
+def is_ip_internal(ip: str):
     try:
         return ipaddress.ip_address(ip).is_private
-    except:
+    except ValueError:
+        logs.DEBUG(f"Not a correct IP: {ip}")
         return False
+
+
+def os_name():
+    return platform.system()
+
+
+def execute_command(x: [str]):
+    return subprocess.check_output(x).replace(b'\n', b'')
+
+
+def whereis(name: str):
+    if os_name() == "Windows":
+        return []
+    try:
+        output = execute_command(["whereis", name])
+    except FileNotFoundError:
+        return []
+    return [str(x) for x in output.split()[1:]]
